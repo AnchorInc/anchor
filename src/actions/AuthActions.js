953d98512e5f67c506bdccf976907d8afb46d5fe
spyking <raghv.indr@gmail.com>
2017-06-25 04:20:54 +0530

    Fixed action creators and reducers

--- src/actions/AuthActions.js
+++ src/actions/AuthActions.js
@@ -1,30 +1,11 @@
 import firebase from 'firebase';
 import {
-<<<<<<< HEAD
   LOGIN_USER_SUCCESS,
   LOGIN_USER_FAIL,
 } from './types';
 
 export const loginUserWithFB = () => {
   return (dispatch) => {
-
-=======
-  EMAIL_CHANGED,
-  PASSWORD_CHANGED,
-  LOGIN_USER_SUCCESS,
-  LOGIN_USER_FAIL,
-  LOGIN_USER,
-} from './types';
-
-const provider = new firebase.auth.GoogleAuthProvider();
-
-// provider.setCustomParameters({
-//   login_hint: 'user@example.com',
-// });
-
-export const loginUserWithFB = () => {
-  return (dispatch) => {
->>>>>>> c15d23b828db3439d889efa237add9190378540c
   };
 };
 
