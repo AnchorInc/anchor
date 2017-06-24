a1ad8afdc877640c28c576dcbbc86eae1f790235
spyking <raghv.indr@gmail.com>
2017-06-25 04:13:40 +0530

    Add reducers and action creators

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import firebase from 'firebase';
 import { setCustomText } from 'react-native-global-props';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
+import reducers from './reducers';
 import LoginForm from './components/screens/LoginForm';
 import MainScreen from './components/screens/MainScreen';
 
-console.disableYellowBox=true;
+console.disableYellowBox = true;
 
 const customTextProps = {
   style: {
@@ -14,9 +19,24 @@ const customTextProps = {
 setCustomText(customTextProps);
 
 class App extends Component {
+  componentWillMount() {
+    const config = {
+    apiKey: 'AIzaSyBJlKq6rsEVUQlaJOrFYKJc15HLWpHjMII',
+    authDomain: 'anchorapp-feed3.firebaseapp.com',
+    databaseURL: 'https://anchorapp-feed3.firebaseio.com',
+    projectId: 'anchorapp-feed3',
+    storageBucket: 'anchorapp-feed3.appspot.com',
+    messagingSenderId: '489771714033',
+  };
+  firebase.initializeApp(config);
+  }
   render() {
+    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk))
+
     return (
-      <LoginForm />
+      <Provider store={store} >
+        <LoginForm />
+      </Provider>
     );
   }
 }
---
+++ src/actions/AuthActions.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase';
+import {
+  LOGIN_USER_SUCCESS,
+  LOGIN_USER_FAIL,
+} from './types';
+
+export const loginUserWithFB = () => {
+  return (dispatch) => {
+
+  };
+};
+
+const loginUserFail = (dispatch) => {
+  dispatch({ type: LOGIN_USER_FAIL });
+};
+
+const loginUserSuccess = (dispatch, result) => {
+  dispatch({ type: LOGIN_USER_SUCCESS, payload: { user: result.user, token: result.credential.token } });
+};
