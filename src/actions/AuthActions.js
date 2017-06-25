b580f17d3c9210b5621c33bce7634f999bbfdf1b
spyking <raghv.indr@gmail.com>
2017-06-25 20:16:35 +0530

    Integrated Facebook login

--- src/actions/AuthActions.js
+++ src/actions/AuthActions.js
@@ -1,11 +1,36 @@
 import firebase from 'firebase';
+import FBSDK from 'react-native-fbsdk';
 import {
   LOGIN_USER_SUCCESS,
   LOGIN_USER_FAIL,
 } from './types';
 
+const provider = firebase.auth.FacebookAuthProvider;
+const { LoginManager, AccessToken } = FBSDK;
+
 export const loginUserWithFB = () => {
   return (dispatch) => {
+    LoginManager.logInWithReadPermissions(['public_profile', 'email'])
+    .then(result => {
+      if (result.isCancelled) {
+        console.log('user canceled');
+        return;
+      }
+
+      AccessToken.getCurrentAccessToken()
+      .then((data) => {
+        const credential = provider.credential(data.accessToken);
+        return firebase.auth().signInWithCredential(credential);
+      })
+      .then((loginData) => {
+        console.log(loginData);
+        console.log('login successful!');
+      })
+      .catch((error) => {
+        console.log(error);
+        console.log('login unsuccessful!');
+      });
+    });
   };
 };
 
