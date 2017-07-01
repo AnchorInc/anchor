97d5d12deabdc3a3aaa6ada8b7086e6dace639c9
spyking <raghv.indr@gmail.com>
2017-07-01 23:30:01 +0530

    Changed status bar color to the correct color

--- app/components/screens/SplashScreen.js
+++ app/components/screens/SplashScreen.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import firebase from 'firebase';
-import { View, Text, Image, Dimensions, AsyncStorage } from 'react-native';
-import { MAIN_COLOR, PROVIDER_FB, PROVIDER_GOOGLE } from '../../config';
+import { View, Text, Image, Dimensions, AsyncStorage, StatusBar} from 'react-native';
+import { MAIN_COLOR, STATUS_BAR_COLOR, PROVIDER_FB, PROVIDER_GOOGLE } from '../../config';
 
 const logo = require('../../resources/images/splashScreenLogo.png');
 
@@ -46,6 +46,7 @@ class SplashScreen extends Component {
   render() {
     return (
       <View style={styles.viewStyle}>
+        <StatusBar backgroundColor={STATUS_BAR_COLOR} />
         <Image source={logo} style={styles.logoStyle} />
         <Text style={styles.textStyle}>Start Learning With Anchor</Text>
         {this.checkForUser()}
