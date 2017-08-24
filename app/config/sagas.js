5edf90fd64c980f5ab582df9a11597ae929776cd
spyking <raghv.indr@gmail.com>
2017-08-24 18:37:24 +0530

    Fix user login and authentication code

--- app/config/sagas.js
+++ app/config/sagas.js
@@ -2,71 +2,61 @@ import { put, takeLatest, fork } from 'redux-saga/effects';
 import firebase from 'firebase';
 import FBSDK from 'react-native-fbsdk';
 import { GoogleSignin } from 'react-native-google-signin';
-import {
-  LOGIN_USER_SUCCESS,
-  LOGIN_USER_FAIL,
-  START_AUTH,
-  GOOGLE_LOGIN_REQUEST,
-  FB_LOGIN_REQUEST,
-} from './';
-import { showErrorMessage } from '../actions';
-
-const loginUserFail = (error) => {
-  return { type: LOGIN_USER_FAIL, payload: error };
-};
-
-const loginUserSuccess = () => {
-  return { type: LOGIN_USER_SUCCESS };
-};
-
-const startAuth = () => {
-  return { type: START_AUTH };
-};
+import { GOOGLE_LOGIN_REQUEST, FB_LOGIN_REQUEST } from './';
+import { showErrorMessage, startAuth, loginUserSuccess, loginUserFail } from '../actions';
 
 // worker Saga: will be called on GOOGLE_LOGIN_REQUESTED actions
 function* loginUserWithGoogle() {
-  const googleProvider = firebase.auth.GoogleAuthProvider;
-
-  yield GoogleSignin.signIn()
-  .then((user) => {
-    put(startAuth());
+  try {
+    const googleProvider = firebase.auth.GoogleAuthProvider;
+    const user = yield GoogleSignin.signIn();
+    yield put(startAuth());
     const credential = googleProvider.credential(user.idToken);
-    return firebase.auth().signInWithCredential(credential);
-  })
-  .then(() => {
-    put(loginUserSuccess());
-  })
-  .catch((error) => {
+    yield firebase.auth().signInWithCredential(credential)
+    yield put(loginUserSuccess());
+  } catch (error) {
     // Error handling for login cancellation by user
+    yield put(loginUserFail());
     if (error.code !== 12501) {
-      put(loginUserFail());
-      put(showErrorMessage(error.message));
+      yield put(showErrorMessage(error.message));
     }
-  });
+  }
 }
 
 // worker Saga: will be called on FB_LOGIN_REQUESTED actions
 function* loginUserWithFB() {
-  const fbProvider = firebase.auth.FacebookAuthProvider;
-  const { LoginManager, AccessToken } = FBSDK;
-
-  yield LoginManager.logInWithReadPermissions(['public_profile', 'email'])
-  .then((result) => {
+  try {
+    const fbProvider = firebase.auth.FacebookAuthProvider;
+    const { LoginManager, AccessToken } = FBSDK;
+    const result = yield LoginManager.logInWithReadPermissions(['public_profile', 'email'])
     if (result.isCancelled) return;
-    put(startAuth());
-    AccessToken.getCurrentAccessToken()
-    .then((user) => {
-      const credential = fbProvider.credential(user.accessToken);
-      return firebase.auth().signInWithCredential(credential);
-    })
-    .then(() => {
-      put(loginUserSuccess());
-    });
-  })
-  .catch((error) => {
-    put(loginUserFail());
-    put(showErrorMessage(error.message));
-  });
+    yield put(startAuth());
+    const user = yield AccessToken.getCurrentAccessToken();
+    const credential = fbProvider.credential(user.accessToken);
+    yield firebase.auth().signInWithCredential(credential)
+    yield put(loginUserSuccess());
+  } catch (error) {
+    yield put(loginUserFail());
+    yield put(showErrorMessage(error.message));
+  }
+
+  // yield LoginManager.logInWithReadPermissions(['public_profile', 'email'])
+  // .then((result) => {
+  //   if (result.isCancelled) return;
+  //   put(startAuth());
+  //   AccessToken.getCurrentAccessToken()
+  //   .then((user) => {
+  //     const credential = fbProvider.credential(user.accessToken);
+  //     return firebase.auth().signInWithCredential(credential);
+  //   })
+  //   .then(() => {
+  //     put(loginUserSuccess());
+  //   });
+  // })
+  // .catch((error) => {
+  //   put(loginUserFail());
+  //   put(showErrorMessage(error.message));
+  // });
 }
 
 function* watchLoginRequests() {
