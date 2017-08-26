7c1eea8a3c4b0a3af651d8243ce8271b3c2b2f82
spyking <raghv.indr@gmail.com>
2017-08-26 23:48:02 +0530

    Remove react-native-material-bottom-navigation and use BottomBar from react-navigation instead

--- app/navigation/Router.js
+++ app/navigation/Router.js
@@ -1,39 +1,25 @@
 import React from 'react';
 
-import { TabNavigator, StackNavigator, NavigationActions } from 'react-navigation';
-import { NavigationComponent } from 'react-native-material-bottom-navigation';
+import { TabNavigator, StackNavigator, NavigationActions, TabBarBottom } from 'react-navigation';
 import Icon from 'react-native-vector-icons/MaterialIcons';
-import { MAIN_COLOR, BOTTOM_BAR_COLOR, BOTTOM_BAR_ICON_COLOR } from '../config';
+import { BOTTOM_BAR_ICON_COLOR, BOTTOM_BAR_COLOR } from '../config';
 import { Classes, Settings, Search, AppSetup, Preferences, Main } from '../components/screens';
-import { Header } from '../components/common';
+// import { Header } from '../components/common';
 import Login from '../components/screens/Login';
 
+const LoginStackConfig = {
+  navigationOptions: {
+    header: null,
+  },
+  headerMode: 'none',
+};
+
 export const LoginStack = StackNavigator({
-  AppSetup: {
-    screen: AppSetup,
-    navigationOptions: {
-      header: null,
-    },
-  },
-  Login: {
-    screen: Login,
-    navigationOptions: {
-      header: null,
-    },
-  },
-  Preferences: {
-    screen: Preferences,
-    navigationOptions: {
-      header: <Header title='Preferences' prefButtons />,
-    },
-  },
-  Main: {
-    screen: Main,
-    navigationOptions: {
-      header: null,
-    },
-  },
-});
+  AppSetup: { screen: AppSetup },
+  Login: { screen: Login },
+  Preferences: { screen: Preferences },
+  Main: { screen: Main },
+}, LoginStackConfig);
 
 const defaultGetStateForAction = LoginStack.router.getStateForAction;
 
@@ -54,6 +40,22 @@ LoginStack.router.getStateForAction = (action, state) => {
     state.routes[state.index].routeName !== 'Preferences')
   ) { return null; }
   return defaultGetStateForAction(action, state);
+};
+
+const TabNavigatorConfig = {
+  tabBarComponent: TabBarBottom,
+  tabBarPosition: 'bottom',
+  swipeEnabled: true,
+  animationEnabled: false,
+  tabBarOptions: {
+    showLabel: true,
+    labelStyle: {
+      fontSize: 12,
+    },
+    tabStyle: {
+      backgroundColor: BOTTOM_BAR_COLOR,
+    },
+   },
 };
 
 export const Tabs = TabNavigator({
@@ -78,32 +80,4 @@ export const Tabs = TabNavigator({
       tabBarIcon: () => <Icon size={24} name="settings" color={BOTTOM_BAR_ICON_COLOR} />,
     },
   },
-}, {
-  tabBarComponent: NavigationComponent,
-  tabBarPosition: 'bottom',
-  swipeEnabled: false,
-  tabBarOptions: {
-    bottomNavigationOptions: {
-      labelColor: BOTTOM_BAR_ICON_COLOR,
-      rippleColor: MAIN_COLOR,
-      shifting: false,
-      tabs: {
-        Search: {
-          barBackgroundColor: BOTTOM_BAR_COLOR,
-          activeIcon: <Icon size={24} name="search" color={MAIN_COLOR} />,
-          activeLabelColor: MAIN_COLOR,
-        },
-        Classes: {
-          barBackgroundColor: BOTTOM_BAR_COLOR,
-          activeIcon: <Icon size={24} name="list" color={MAIN_COLOR} />,
-          activeLabelColor: MAIN_COLOR,
-        },
-        Settings: {
-          barBackgroundColor: BOTTOM_BAR_COLOR,
-          activeIcon: <Icon size={24} name="settings" color={MAIN_COLOR} />,
-          activeLabelColor: MAIN_COLOR,
-        },
-      },
-    },
-  },
-});
+}, TabNavigatorConfig);
