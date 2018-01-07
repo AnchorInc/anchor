23c3d35c4e1b66180cc256191f9452bd8d2fbc3d
SpyKing <raghv.indr@gmail.com>
2018-01-07 20:50:59 +0530

    remove editprofile styles

--- app/components/screens/ProfileEditing.js
+++ app/components/screens/ProfileEditing.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Modal, Dimensions, TouchableOpacity, Image, Text, StatusBar } from 'react-native';
+import { View, Dimensions, TouchableOpacity, Image, Text, StatusBar } from 'react-native';
 import { connect } from 'react-redux';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import { MAIN_COLOR, ACCENT_COLOR } from '../../config';
@@ -23,8 +23,6 @@ class ProfileEditing extends Component {
       headerIconStyle,
       profPicStyle,
       profImageStyle,
-      editProfPicStyle,
-      editProfStyle,
       profPicContainerStyle,
       textStyle,
     } = styles;
@@ -47,9 +45,6 @@ class ProfileEditing extends Component {
             <View style={profPicStyle}>
               <Image source={{ uri: this.props.user.photoURL }} style={profImageStyle} />
             </View>
-            <TouchableOpacity onPress={this.showPhotoEditMenu} style={editProfPicStyle}>
-              <Text style={editProfStyle}>EDIT IMAGE</Text>
-            </TouchableOpacity>
             <Text style={textStyle}>
               {this.props.user.displayName}
             </Text>
@@ -71,21 +66,6 @@ const styles = {
     paddingTop: 5,
     flex: 1,
   },
-  editProfPicStyle: {
-    width: 0.35 * width,
-    height: 35,
-    backgroundColor: ACCENT_COLOR,
-    borderRadius: 45,
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 10,
-  },
-  editProfStyle: {
-    color: 'white',
-    fontFamily: 'avenir_light',
-    fontSize: 14,
-    textAlign: 'center',
-  },
   headerTextStyle: {
     fontSize: 20,
     fontWeight: 'bold',
@@ -99,27 +79,29 @@ const styles = {
     paddingRight: 0.05 * width,
   },
   profPicContainerStyle: {
-    flex: 4,
+    flex: 3,
     justifyContent: 'space-between',
     alignItems: 'center',
+    paddingBottom: 30,
   },
   profPicStyle: {
-    width: 105,
-    height: 105,
+    width: width * 0.3,
+    height: width * 0.3,
     backgroundColor: 'rgba(255,255,255,0.4)',
-    borderRadius: 52.5,
+    borderRadius: 100,
     justifyContent: 'center',
     alignItems: 'center',
   },
   profImageStyle: {
-    width: 90,
-    height: 90,
+    width: width * 0.27,
+    height: width * 0.27,
     borderRadius: 100,
   },
   textStyle: {
     fontSize: 20,
     color: 'white',
-    paddingBottom: 20,
+    paddingRight: 20,
+    paddingLeft: 20,
   },
 };
 
