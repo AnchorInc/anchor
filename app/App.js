7b4c7a1e66dc907164176ef06105ae072adc1d2e
spyking <raghv.indr@gmail.com>
2018-05-18 19:46:54 +0530

    remove firebase and rsf

--- app/App.js
+++ app/App.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
-import firebase from 'firebase';
 import { GoogleSignin } from 'react-native-google-signin';
 import { setCustomText, setCustomStatusBar } from 'react-native-global-props';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
-import ReduxSagaFirebase from 'redux-saga-firebase';
 import rootSaga from './sagas';
 import reducers from './reducers';
 import { MainStack } from './navigation/Router';
@@ -22,27 +20,10 @@ const customTextProps = {
 setCustomText(customTextProps);
 setCustomStatusBar({ backgroundColor: colors.primary.dark });
 
-const config = {
-  apiKey: 'AIzaSyCGttWR24ng1Y87ruWfjAcGCISGLKz8jUE',
-  authDomain: 'anchorapp-feed3.firebaseapp.com',
-  databaseURL: 'https://anchorapp-feed3.firebaseio.com',
-  projectId: 'anchorapp-feed3',
-  storageBucket: 'anchorapp-feed3.appspot.com',
-  messagingSenderId: '489771714033',
-};
-let firebaseApp;
-try {
-  firebaseApp = firebase.initializeApp(config);
-} catch (err) {
-  console.log(err);
-}
-
 GoogleSignin.configure({
   webClientId: gsigninConfig.webClientId,
 });
 
-const rsf = new ReduxSagaFirebase(firebaseApp);
-export { rsf };
 const middleware = createSagaMiddleware();
 const store = createStore(reducers, {}, applyMiddleware(middleware));
 middleware.run(rootSaga);
