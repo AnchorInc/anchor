14cb1444942c20fe512a87977f829a5ace876d00
Akash <akash.munagala@gmail.com>
2018-06-27 20:05:13 +0530

    Fix TouchableDebounce
    Earlier edit made only the first tap work

--- app/lib/TouchableDebounce.js
+++ app/lib/TouchableDebounce.js
@@ -2,16 +2,23 @@ import React, { Component } from 'react';
 import { TouchableOpacity } from 'react-native';
 
 class TouchableDebounce extends Component {
-  state = { disabled : false};
+  state = { disabled: false };
+  componentWillMount() {
+    console.log(1);
+    this.setState({ disabled: false });
+  }
+
   render() {
+    console.log(1);
     return (
       <TouchableOpacity
         style={this.props.style}
         activeOpacity={this.props.activeOpacity}
         disabled={this.state.disabled}
         onPress={() => {
-            this.props.onPress()
-            this.setState({ disabled: true })
+            this.props.onPress();
+            this.setState({ disabled: true });
+            setInterval(() => { this.setState({ disabled: false }); }, 500);
           }
         }
       >
