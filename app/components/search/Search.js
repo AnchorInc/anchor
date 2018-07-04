AnchorInc/anchor

commit 6f4b6f244745150bfde3ded92d2684e8318a6fab

fix fcm token listener saga error

app/sagas/UserSagas.js
@@ -71,8 +71,8 @@ const userEventListener = (ref) => {
 };
 
 function* fcmTokenSaga() {
-  let token = yield call([firebase.messaging(), firebase.messaging().getToken]);
-  yield call(updateUserSaga, { user: { fcmToken: token } });
+  // let token = yield call([firebase.messaging(), firebase.messaging().getToken]);
+  // yield call(updateUserSaga, { user: { fcmToken: token } });
 
   const channel = yield call(fcmTokenRefreshListener);
   while (firebase.auth().currentUser) {

app/sagas/AuthSagas.js
@@ -3,9 +3,8 @@ import { put, takeLatest, all, call } from 'redux-saga/effects';
 import firebase from 'react-native-firebase';
 import { AccessToken, LoginManager } from 'react-native-fbsdk';
 import { GoogleSignin } from 'react-native-google-signin';
-import { eventChannel } from 'redux-saga';
 
-import { showSpinner, loginSuccess, loginFail, syncUser } from '../actions';
+import { showSpinner, loginSuccess, loginFail } from '../actions';
 import { actionTypes, firebasePaths, userTypes, signinMethods } from '../config';
 
 
@@ -23,8 +22,6 @@ function* loginUserWithGoogle(action) {
     // sign in to firebase and get the user credentials
 
     yield call(initUser, action, userCred);
-
-    yield put(loginSuccess());
   } catch (error) {
     // Error handling for login cancellation by user
     if (error.code !== 12501) {
@@ -83,6 +80,7 @@ function* logoutUser() {
 }
 
 function* initUser(action, userCred) {
+  console.log('init');
   // get a db reference to the user
   const userCollection = getUserCollection(action);
   const user = userCred.user._user;
@@ -113,6 +111,11 @@ function* initUser(action, userCred) {
     [['user_collection', userCollection],
     ['user_data', JSON.stringify(userData)],
     ['signin_method', JSON.stringify(action.method)]]);
+
+  const collection = yield call([AsyncStorage, AsyncStorage.getItem], 'user_collection');
+  console.log(JSON.parse(collection));
+
+  yield put(loginSuccess());
 }
 
 const getUserCollection = (action) => {

commit b92ceff1a31053d81f1919a82bf99ce8e66e366f

Fix Getting FCM Token caused by 6f4b6f244745150bfde3ded92d2684e8318a6fab

app/components/search/Search.js
@@ -16,7 +16,7 @@ class Search extends Component {
   };
 
   onPress = (uid) => {
-    console.log(uid);+
+    console.log(uid);
     
     this.props.navigation.navigate('TeacherProfile', { uid, action: 'forum' });
   }

app/sagas/UserSagas.js
@@ -71,9 +71,13 @@ const userEventListener = (ref) => {
 };
 
 function* fcmTokenSaga() {
+  let token = yield call([firebase.messaging(), firebase.messaging().getToken]);
+  console.log("FCM Token: ", token);
+  yield call(updateUserSaga, { user: { fcmToken: token } }); 
+
   const channel = yield call(fcmTokenListener);
   while (firebase.auth().currentUser) {
-    const token = yield take(channel);
+    token = yield take(channel);
     yield call(updateUserSaga, { user: { fcmToken: token } });
   }
   channel.close();