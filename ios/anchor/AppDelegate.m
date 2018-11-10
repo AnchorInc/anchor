daa352a863423984217f49d0f6a148f56bedcf07
spyking <spyking@spykings-MacBook-Pro.local>
2018-11-10 16:28:08 +0530

    update ios

--- ios/anchor/AppDelegate.m
+++ ios/anchor/AppDelegate.m
@@ -83,5 +83,14 @@ completionHandler:(void (^ __nullable)(BOOL success))completion  {
   || [RNGoogleSignin application:application openURL:url sourceApplication:options[UIApplicationOpenURLOptionsSourceApplicationKey] annotation:options[UIApplicationOpenURLOptionsAnnotationKey]];
 }
 
+- (void)setStatusBarBackgroundColor:(UIColor *)color {
+  
+  UIView *statusBar = [[[UIApplication sharedApplication] valueForKey:@"statusBarWindow"] valueForKey:@"statusBar"];
+  
+  if ([statusBar respondsToSelector:@selector(setBackgroundColor:)]) {
+    statusBar.backgroundColor = color;
+  }
+}
+
 @end
   
