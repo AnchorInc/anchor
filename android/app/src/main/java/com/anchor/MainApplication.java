50550c3b7d45f8683cedbfe0b48b88d510d92d5c
spyking <raghv.indr@gmail.com>
2019-01-15 23:32:33 +0530

    fix syntax error

--- android/app/src/main/java/com/anchor/MainApplication.java
+++ android/app/src/main/java/com/anchor/MainApplication.java
@@ -70,7 +70,7 @@ public class MainApplication extends Application implements ReactApplication {
           new VectorIconsPackage(),
           new FBSDKPackage(mCallbackManager),
 
-          new AndroidKeyboardAdjustPackage(),
+          new AndroidKeyboardAdjustPackage()
       );
     }
 
