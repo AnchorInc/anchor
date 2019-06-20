176e514af20e989b2301815ac3728d6067931b6b
Spyking <raghv.indr@gmail.com>
2019-06-20 13:30:46 +0530

    update

--- app/components/chat/Chat.js
+++ app/components/chat/Chat.js
@@ -1,4 +1,4 @@
-  import React, { Component } from 'react';
+import React, { Component } from 'react';
 import { View, FlatList, Platform } from 'react-native';
 import { connect } from 'react-redux';
 import firebase from 'react-native-firebase';
